e31400a060307a5fad81ec880aab448c537bf8df
Anderson Soares Furtado Oliveira <60579268+andersonfurtado@users.noreply.github.com>
2021-01-07 19:52:01 -0300

    Update score.py

--- score.py
+++ score.py
@@ -1,5 +1,30 @@
-input_sample = pd.DataFrame({"Pregnancies": pd.Series([0.0], dtype="float64"), "Glucose": pd.Series([0.0], dtype="float64"), "BloodPressure": pd.Series([0.0], dtype="float64"), "SkinThickness": pd.Series([0.0], dtype="float64"), "Insulin": pd.Series([0.0], dtype="float64"), "BMI": pd.Series([0.0], dtype="float64"), "DiabetesPedigreeFunction": pd.Series([0.0], dtype="float64"), "Age": pd.Series([0.0], dtype="float64")})
+import json
+import logging
+import os
+import pickle
+import numpy as np
+import pandas as pd
+import joblib
+
+import azureml.automl.core
+from azureml.automl.core.shared import logging_utilities, log_server
+from azureml.telemetry import INSTRUMENTATION_KEY
+
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
+
+
+input_sample = pd.DataFrame({"Sigla_UF": pd.Series(["example_value"], dtype="object"), "Cod_Municipio": pd.Series([0.0], dtype="float64"), "Nome_Municipio": pd.Series(["example_value"], dtype="object"), "Cod_Escola": pd.Series([0.0], dtype="float64"), "Nome_Escola": pd.Series(["example_value"], dtype="object"), "Column6": pd.Series(["example_value"], dtype="object"), "TAprov2019_Total": pd.Series([0.0], dtype="float64"), "TAprov2019_1_serie": pd.Series(["example_value"], dtype="object"), "TAprov2019_2_serie": pd.Series(["example_value"], dtype="object"), "TAprov2019_3_serie": pd.Series([0.0], dtype="float64"), "TAprov2019_4_serie": pd.Series(["example_value"], dtype="object"), "Indicador_Rendimento": pd.Series([0.0], dtype="float64"), "SAEB2019_Matematica": pd.Series([0.0], dtype="float64"), "SAEB2019_Lingua Portuguesa": pd.Series([0.0], dtype="float64"), "SAEB2019_Nota Media Padronizada": pd.Series([0.0], dtype="float64")})
 output_sample = np.array([0])
+try:
+    log_server.enable_telemetry(INSTRUMENTATION_KEY)
+    log_server.set_verbosity('INFO')
+    logger = logging.getLogger('azureml.automl.core.scoring_script')
+except:
+    pass
+
+
 def init():
     global model
     # This name is model.id of model that we want to deploy deserialize the model file back
