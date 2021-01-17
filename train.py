6366d975cf7e48576b7693be1ec16366d1452246
Anderson Soares Furtado Oliveira <60579268+andersonfurtado@users.noreply.github.com>
2021-01-16 22:24:14 -0300

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 import argparse
 import os
 import numpy as np
@@ -15,8 +15,8 @@ from azureml.core.run import Run
 
 run = Run.get_context()
 
-url = 'https://github.com/andersonfurtado/Capstone/blob/master/data/data.CSV'
-dataset = TabularDatasetFactory.from_delimited_files(url)
+ideb_dataset = Dataset.Tabular.from_delimited_files("https://raw.githubusercontent.com/andersonfurtado/Capstone/master/data/data.CSV", separator=';', encoding= 'latin1')
+data = ideb_dataset.to_pandas_dataframe()
 
 def clean_data(data):
    
@@ -38,29 +38,21 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
     
-    #parser.add_argument("--C", type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    #parser.add_argument("--max_iter", type=int, default=100, help="Maximum number of iterations to converge")
-
-    parser.add_argument("--kernel", type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument("--penalty", type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument("--C", type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument("--max_iter", type=int, default=100, help="Maximum number of iterations to converge")
 
     primary_metric_name='r2_score'
     args = parser.parse_args()
 
-    #run.log("Regularization Strength:", np.float(args.C))
-    #run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
 
-    run.log("Regularization Strength:", np.float(args.kernel))
-    run.log("Max iterations:", np.int(args.penalty))
 
-    #model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-    #joblib.dump(model,'outputs/model.joblib')
-    #accuracy = model.score(x_test, y_test)
 
-    model = LogisticRegression(kernel=args.kernel, penalty=args.penalty).fit(x_train, y_train)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
     joblib.dump(model,'outputs/model.joblib')
     accuracy = model.score(x_test, y_test)
     
-    run.log('r2_score', np.float(accuracy))
+    run.log('r2_score', np.float(r2_score))
 if __name__ == '__main__':
     main()
